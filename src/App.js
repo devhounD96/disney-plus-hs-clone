8b5559f0294cd17faf47fec58f416057056f16a3
vishnuSrishti <vishnusekhar.sics@gmail.com>
2022-07-28 18:53:57 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import { Routes, Route} from 'react-router-dom'
+import Login from './components/Login';
 
 function App() {
   return (
-    <div>
-     
-    </div>
+    <>
+    <Routes>
+      <Route path='/' element={<Login />}>
+      </Route>
+    </Routes>
+    </>
   );
 }
 
