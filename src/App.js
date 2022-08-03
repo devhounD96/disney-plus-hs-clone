b6e9d7fb54cd2f315e496205577bf1d3033bd085
vishnu <vishnusekhar.sics@gmail.com>
2022-08-03 20:37:24 +0530

    working on detail page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route} from 'react-router-dom'
 import Login from './components/Login';
 import Header from './components/Header';
 import Home from './components/Home';
+import Detail from './components/Detail';
 
 function App() {
   return (
@@ -12,6 +13,8 @@ function App() {
       <Route path='/' element={<Login />}>
       </Route>
       <Route path='/home' element={<Home />}>
+      </Route>
+      <Route path='/detail/:id' element={<Detail />}>
       </Route>
     </Routes>
     </>
